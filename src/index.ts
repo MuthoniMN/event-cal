a03ad3dd97c4553ab45a93909fdcafd506bba4d1
Mitch <ndianguimichelle@gmail.com>
2025-01-02 15:39:26 +0300

    feat: homepage and auth views

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
+import { config } from "dotenv";
 
-require("dotenv").config()
+config();
 const app = express();
 const port = process.env.PORT;
 
@@ -9,7 +10,16 @@ app.use(express.json())
 app.use(express.static("public"))
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Fully set up!");
+  res.render("index");
 })
+
+app.get("/signup", (req: Request, res: Response) => {
+  res.render("pages/signup");
+})
+
+app.get("/login", (req: Request, res: Response) => {
+  res.render("pages/login");
+})
+
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`))
